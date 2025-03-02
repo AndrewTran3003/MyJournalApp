60ff1a88f39da0129d8dc61b00b54b07e195892f
Tuan (Andrew) Tran <37162093+AndrewTran3003@users.noreply.github.com>
2025-03-02 18:40:01 +1100

    add another test to test removing the field

--- QuestionAndAnswerUITests/FormNavigationUITests.swift
+++ QuestionAndAnswerUITests/FormNavigationUITests.swift
@@ -116,6 +116,46 @@ class FormNavigationUITests: XCTestCase {
         XCTAssertEqual(removeButtonCount, 0, "Should have exactly 0 remove button")
 
     }
+
+    func testShouldRemoveTheRightField() throws {
+        // Arrange and Act
+        // Create two fields. First field to have the field name "Field to be removed". Second field to have the field name "Field to stay"
+        // Then click on the remove button of the first field
+        app.tabBars.buttons["Forms"].tap()
+        app.buttons["Create a new form"].tap()
+
+        let addButton = app.descendants(matching: .button)
+            .matching(NSPredicate(format: "identifier == 'plus.circle.fill'")).firstMatch
+        addButton.tap()
+
+        let fieldNameInputs = app.textFields.matching(identifier: "Enter the field name here")
+            .allElementsBoundByIndex
+
+        fieldNameInputs[0].tap()
+        fieldNameInputs[0].typeText("Field to be removed")
+
+        fieldNameInputs[1].tap()
+        fieldNameInputs[1].typeText("Field to stay")
+
+        let removeButtons = app.descendants(matching: .button)
+            .matching(NSPredicate(format: "identifier == 'minus.circle.fill'"))
+            .allElementsBoundByIndex
+        removeButtons[0].tap()
+
+        // Assert
+        let removedFieldCount = app.textFields.matching(
+            NSPredicate(format: "value CONTAINS 'Field to be removed'")
+        ).count
+        XCTAssertEqual(
+            removedFieldCount, 0, "Field with text 'Field to be removed' should not exist")
+
+        let remainingFieldCount = app.textFields.matching(
+            NSPredicate(format: "value CONTAINS 'Field to stay'")
+        ).count
+        XCTAssertEqual(
+            remainingFieldCount, 1, "Should have exactly one field with text 'Field to stay'")
+
+    }
     func testShouldHave3NewFieldViewsWhenTappingTheAddButtonTwice() throws {
         // Arrange
         app.tabBars.buttons["Forms"].tap()
