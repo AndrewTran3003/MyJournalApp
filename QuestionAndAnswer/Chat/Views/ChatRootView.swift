94835293f3aff2999e07271fd5ee9ca457b7c540
Tuan (Andrew) Tran <37162093+AndrewTran3003@users.noreply.github.com>
2025-06-22 14:59:08 +1000

    initial commit for the chat feature

---
+++ QuestionAndAnswer/Chat/Views/ChatRootView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ChatRootView: View {
+    var body: some View {
+        NavigationView {
+            Text("Chat Feature Coming Soon")
+                .navigationTitle("Conversations")
+        }
+    }
+}
+
+struct ChatRootView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatRootView()
+    }
+} 
